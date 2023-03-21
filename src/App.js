81f2b16292d8d713dafdbf8513ab8ae6374013ba
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-21 08:44:48 +0000

    new classes added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ function App() {
   return (
     <div className='App'>
       <div>
-        <Button primary>Click Me!</Button>
+        <Button success rounded outline>
+          Click Me!
+        </Button>
       </div>
       <div>
-        <Button>Buy Now!</Button>
+        <Button danger outline>
+          Buy Now!
+        </Button>
       </div>
       <div>
-        <Button>See Deal!</Button>
+        <Button warning>See Deal!</Button>
       </div>
       <div>
-        <Button>Hide Ads!</Button>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
       </div>
       <div>
-        <Button>Something!</Button>
+        <Button primary rounded>
+          Something!
+        </Button>
       </div>
     </div>
   )
--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,5 @@
+import className from 'classnames'
+
 const Button = ({
   children,
   primary,
@@ -8,12 +10,15 @@ const Button = ({
   outline,
   rounded,
 }) => {
-  let baseClassName = 'px-3 py-1.5 border'
-  if (primary) {
-    baseClassName += 'border-blue-500 bg-blue-500 text-white'
-  }
+  const classes = className('px-3 py-1.5 border', {
+    'border-blue-500 bg-blue-500 text-white': primary,
+    'border-gray-900 bg-gray-900 text-white': secondary,
+    'border-green-500 bg-green-500 text-white': success,
+    'border-yellow-400 bg-yellow-400 text-white': warning,
+    'border-red-500 bg-red-500 text-white': danger,
+  })
 
-  return <button className={baseClassName}>{children}</button>
+  return <button className={classes}>{children}</button>
 }
 
 export default Button
