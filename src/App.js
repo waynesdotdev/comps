76efc8cf02f30d71e81d6d8911d72eae99b24aa7
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-21 13:13:12 +0000

    udpated imports, made accordion page work

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import Accordion from './components/Accordion'
-
-// import ButtonPage from './pages/ButtonPage'
+import ButtonPage from './pages/ButtonPage'
+import AccordionPage from './pages/AccordionPage'
 
 function App() {
-  const items = [
-    {
-      id: 'dsghf',
-      label: 'Can i use React on a project',
-      content:
-        'You can use it on anything you wants. You can use it on anything you wants.You can use it on anything you wants.You can use it on anything you wants.',
-    },
-    {
-      id: 'hjashfka',
-      label: 'Can i use JavaScript on a project',
-      content:
-        'You can use it on anything you wants. You can use it on anything you wants.You can use it on anything you wants.You can use it on anything you wants.',
-    },
-    {
-      id: 'ldjkfnched',
-      label: 'Can i use CSS on a project',
-      content:
-        'You can use it on anything you wants. You can use it on anything you wants.You can use it on anything you wants.You can use it on anything you wants.',
-    },
-  ]
-
   return (
     <div>
-      {/* <ButtonPage /> */}
-      <Accordion items={items} />
+      <ButtonPage />
+      <AccordionPage />
     </div>
   )
 }
--- src/pages/AccordionPage.js
+++ src/pages/AccordionPage.js
@@ -1,5 +1,33 @@
-const AccordionPage = () => {
-  return <div>AccordionPage</div>
+import Accordion from '../components/Accordion'
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 'dsghf',
+      label: 'Can i use React on a project',
+      content:
+        'You can use it on anything you wants. You can use it on anything you wants.You can use it on anything you wants.You can use it on anything you wants.',
+    },
+    {
+      id: 'hjashfka',
+      label: 'Can i use JavaScript on a project',
+      content:
+        'You can use it on anything you wants. You can use it on anything you wants.You can use it on anything you wants.You can use it on anything you wants.',
+    },
+    {
+      id: 'ldjkfnched',
+      label: 'Can i use CSS on a project',
+      content:
+        'You can use it on anything you wants. You can use it on anything you wants.You can use it on anything you wants.You can use it on anything you wants.',
+    },
+  ]
+
+  return (
+    <div>
+      {/* <ButtonPage /> */}
+      <Accordion items={items} />
+    </div>
+  )
 }
 
 export default AccordionPage
