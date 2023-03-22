0703c62a307b7b708a8b41b4221ba1841d3efdd4
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-22 12:46:59 +0000

    dropdown done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import ButtonPage from './pages/ButtonPage'
-import AccordionPage from './pages/AccordionPage'
+// import ButtonPage from './pages/ButtonPage'
+// import AccordionPage from './pages/AccordionPage'
+// import Dropdown from './components/Dropdown'
 
 function App() {
   return (
     <div>
+      {/* 
       <ButtonPage />
       <AccordionPage />
+      <Dropdown />
+       */}
     </div>
   )
 }
---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import Dropdown from '../components/Dropdown'
+
+function DropdownPage() {
+  const [selection, setSelection] = useState(null)
+
+  const handleSelect = option => {
+    setSelection(option)
+  }
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+  ]
+
+  return (
+    <div>
+      {/* <ButtonPage />
+      <AccordionPage /> */}
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  )
+}
+
+export default DropdownPage
