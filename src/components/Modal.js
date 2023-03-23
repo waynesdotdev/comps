83ec4f7713396f70e1a02c7e0538765a5e4610fb
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-23 14:00:49 +0000

    Modal done

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,9 +14,9 @@ const Modal = ({ children, onClose, actionBar }) => {
     <div>
       <div
         onClick={onClose}
-        className='absolute inset-0 bg-gray-300 opacity-80'
+        className='fixed inset-0 bg-gray-300 opacity-80'
       ></div>
-      <div className='absolute inset-40 p-10 bg-white'>
+      <div className='fixed inset-40 p-10 bg-white'>
         <div className='flex flex-col justify-between h-full'>
           {children}
           <div className='flex justify-end'>{actionBar}</div>
